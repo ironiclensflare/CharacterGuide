4c3a6eb33a6d44e5810746c65d163f0eaf84fb33
Stuart Carruthers <stu@ironiclensflare.com>
2015-08-16 16:07:19 +0100

    Initial commit

---
+++ CharacterGuide/CharacterGuideWebsite/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="CharacterGuideWebsite.MvcApplication" Language="C#" %>
---
+++ CharacterGuide/CharacterGuideWebsite/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(CharacterGuideWebsite.Startup))]
+namespace CharacterGuideWebsite
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
